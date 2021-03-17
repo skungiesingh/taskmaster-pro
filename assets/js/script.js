fb35eacb4d9e18f3525f1e9430a89bb29e293c80
Shastri Kungiesingh <ssppksingh347@gamil.com>
2021-03-17 15:36:24 -0400

    added drag and drop for desktop browser and touch screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,13 +103,13 @@ $(".list-group").on("blur", "textarea", function() {
   var text = $(this)
     .val();
 
-// get the parent ul's ida attribute
+  // get the parent ul's ida attribute
   var status = $(this)
     .closest(".list-group")
     .attr("id")
     .replace("list-", "");
 
-// get the task's position in the list of other li elements
+  // get the task's position in the list of other li elements
   var index = $(this)
     .closest(".list-group-item")
     .index();  
@@ -134,16 +134,16 @@ $(".list-group").on("click", "span", function() {
   .text()
   .trim();
 
-// create new input element
+  // create new input element
   var dateInput = $("<input>")
   .attr("type", "text")
   .addClass("form-control")
   .val(date);
 
-// swap out elements
+  // swap out elements
   $(this).replaceWith(dateInput);
 
-// automatically focus on the new element
+  // automatically focus on the new element
   dateInput.trigger("focus");
 });
 
@@ -187,7 +187,77 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+// create draggable items in the columns
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function() {
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function() {
+        var text = $(this)
+          .find("p")
+          .text()
+          .trim();
+
+        var date = $(this)
+          .find("span")
+          .text()
+          .trim();
+
+        // add task data to the temp array as an object
+        tempArr.push({
+          text: text,
+          date: date
+        });
+      });
+
+      console.log(tempArr);
+      },
+  }
+,);
+
+// creat dropable items for trash
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
+// trim down list's ID to match object property
+var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+//update array on tasks object and save
+tasks[arrName] = tempArr;
+saveTasks();
+
+
 // load tasks for the first time
 loadTasks();
-
-
